adf27dcfed5088ae1974e9b6a696bb05bddbac3e
DESKTOP-GG7EUA5\HoYean Lee <ghdusjang8@gmail.com>
2020-11-25 00:40:34 +0900

    ApiLiveData 적용

--- Android/app/src/main/java/com/doubleslas/fifith/alcohol/model/repository/AuthRepository.kt
+++ Android/app/src/main/java/com/doubleslas/fifith/alcohol/model/repository/AuthRepository.kt
@@ -1,13 +1,11 @@
 package com.doubleslas.fifith.alcohol.model.repository
 
-import com.doubleslas.fifith.alcohol.model.network.base.ApiStatus
-import com.doubleslas.fifith.alcohol.model.network.base.MutableRetroLiveData
-import com.doubleslas.fifith.alcohol.model.network.base.RetroLiveData
+import com.doubleslas.fifith.alcohol.model.network.base.*
 import com.google.firebase.auth.AuthCredential
 
 class AuthRepository {
-    fun completeLogin(credential: AuthCredential): RetroLiveData<Any> {
-        val result = MutableRetroLiveData<Any>()
+    fun completeLogin(credential: AuthCredential): ApiLiveData<Any> {
+        val result = MutableApiLiveData<Any>()
         result.value = ApiStatus.Loading
 
         return result
