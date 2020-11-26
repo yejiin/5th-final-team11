44a77b76f4dc66f1e62dc6e5f9b2e8127a9693f3
iustron@naver.com <iustron@naver.com>
2020-11-26 20:21:59 +0900

    [Init]

--- ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/DsFinalApplication.java
+++ ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/DsFinalApplication.java
@@ -2,12 +2,19 @@ package com.doubleslash.fifth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DsFinalApplication {
+public class DsFinalApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DsFinalApplication.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DsFinalApplication.class);
 	}
 
 }
---
+++ ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.doubleslash.fifth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping(value = "/")
+	@ResponseBody
+	public Object statusCheck() {
+		return "Server Status : ON";
+	}
+	
+}
