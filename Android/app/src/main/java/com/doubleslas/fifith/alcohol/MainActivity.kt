7a26af33c085019fffb40d136591167aaf4b60a7
junmin-chang <chjm219@kookmin.ac.kr>
2020-11-29 14:28:58 +0900

    .

--- Android/app/src/main/java/com/doubleslas/fifith/alcohol/MainActivity.kt
+++ Android/app/src/main/java/com/doubleslas/fifith/alcohol/MainActivity.kt
@@ -1,27 +1,24 @@
 package com.doubleslas.fifith.alcohol
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.MutableLiveData
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.doubleslas.fifith.alcohol.model.network.base.ApiStatus
-import com.doubleslas.fifith.alcohol.model.network.base.RetroLiveData
+import com.doubleslas.fifith.alcohol.model.network.base.MutableApiLiveData
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val liveData = RetroLiveData<String>()
+        val liveData = MutableApiLiveData<String>()
 
         liveData.observe(this, Observer {
-            when(it){
+            when (it) {
                 is ApiStatus.Loading -> {
 
                 }
-                is ApiStatus.Success -> {
+                is ApiStatus.Success<*> -> {
 
                 }
                 is ApiStatus.Error -> {
