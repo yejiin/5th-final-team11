dd9a75a4ca8bbe455364d8d6893fb2d1d414d762
junmin-chang <chjm219@kookmin.ac.kr>
2020-12-08 16:23:00 +0900

    AuthRepository 연결

--- Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/auth/LoginViewModel.kt
+++ Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/auth/LoginViewModel.kt
@@ -3,10 +3,12 @@ package com.doubleslas.fifith.alcohol.ui.auth
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.map
+import com.doubleslas.fifith.alcohol.model.repository.AuthRepository
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginViewModel : ViewModel() {
+    private val authRepository by lazy { AuthRepository() }
     private var firebaseAuth = FirebaseAuth.getInstance()
 
     enum class AuthenticationState {
@@ -27,6 +29,7 @@ class LoginViewModel : ViewModel() {
             .addOnCompleteListener {task ->
                 if (task.isSuccessful) {
                     val user = firebaseAuth.currentUser?.displayName
+                    authRepository.completeLogin(credential)
                     Log.d("junmin", user.toString())
                 }
             }
