40ae5269f3a2016e6ef90cf1f8331b73b11ba336
HoYean Lee <ghdusjang8@gmail.com>
2021-02-08 15:59:27 +0900

    정렬 ViewHolder 처리

--- Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/collection/MyReviewAdapter.kt
+++ Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/collection/MyReviewAdapter.kt
@@ -4,20 +4,27 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.doubleslas.fifith.alcohol.databinding.ItemMyReviewBinding
+import com.doubleslas.fifith.alcohol.databinding.ItemSortBinding
 import com.doubleslas.fifith.alcohol.dto.MyReviewData
-import com.doubleslas.fifith.alcohol.ui.common.SortViewHolder
+import com.doubleslas.fifith.alcohol.enum.SearchSortType
 
 class MyReviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     private var list: List<MyReviewData>? = null
+    private var sortType: SearchSortType? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        when (viewType) {
-            ITEM_TYPE_SORT -> SortViewHolder.create(inflater, parent)
+        return when (viewType) {
+            ITEM_TYPE_SORT -> {
+                val binding = ItemSortBinding.inflate(inflater, parent, false)
+                SortViewHolder(binding)
+            }
+            else -> {
+                val binding = ItemMyReviewBinding.inflate(inflater, parent, false)
+                ReviewViewHolder(binding)
+            }
         }
 
-        val binding = ItemMyReviewBinding.inflate(inflater, parent, false)
-        return ReviewViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
@@ -27,7 +34,9 @@ class MyReviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         when (holder) {
-            is SortViewHolder -> holder.bind()
+            is SortViewHolder -> holder.binding.run {
+                tvSort.text = sortType?.text ?: SearchSortType.Popular.text
+            }
             is ReviewViewHolder -> holder.bind(getItem(position))
         }
     }
@@ -55,6 +64,18 @@ class MyReviewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
             binding.tvReview.text = item.review
             binding.tvDate.text = item.date
             binding.tvLocation.text = item.location
+        }
+    }
+
+
+    inner class SortViewHolder(val binding: ItemSortBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+//                val activity = it.context as AppCompatActivity
+//                sortDialog.setInitSort(sortType ?: SearchSortType.Popular)
+//                sortDialog.show(activity.supportFragmentManager, null)
+            }
         }
     }
 
