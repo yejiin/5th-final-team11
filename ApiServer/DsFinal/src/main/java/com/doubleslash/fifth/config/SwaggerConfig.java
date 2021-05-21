6485aade6333502209f301ad17fa28fb7708e594
Son <iustron@naver.com>
2021-05-22 01:18:38 +0900

    feat: swagger path 수정

--- ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/config/SwaggerConfig.java
+++ ApiServer/DsFinal/src/main/java/com/doubleslash/fifth/config/SwaggerConfig.java
@@ -12,6 +12,8 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.doubleslash.fifth.interceptor.AuthInterceptor;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -23,8 +25,6 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import com.doubleslash.fifth.interceptor.AuthInterceptor;
 
 @Configuration
 @EnableSwagger2
@@ -82,7 +82,7 @@ public class SwaggerConfig extends WebMvcConfigurationSupport{
     	String paths[] = {
                 "/user/register",
                 "/user/savepoint",
-                "/review/?????",
+                "/review/*",
                 "/review/*/*",
                 "/review/*/*/*",
                 "/review",
