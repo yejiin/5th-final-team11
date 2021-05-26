80127d1a18137012face493ba0fd211067566761
Lee Ho Yean <comforest@naver.com>
2021-05-27 01:04:15 +0900

    feat: 최소 버전 확인

--- Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/SplashActivity.kt
+++ Android/app/src/main/java/com/doubleslas/fifith/alcohol/ui/SplashActivity.kt
@@ -2,14 +2,16 @@ package com.doubleslas.fifith.alcohol.ui
 
 import android.animation.Animator
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.doubleslas.fifith.alcohol.App
 import com.doubleslas.fifith.alcohol.databinding.ActivitySplashBinding
+import com.doubleslas.fifith.alcohol.model.FirebaseConfig
 import com.doubleslas.fifith.alcohol.ui.auth.AgeCheckActivity
+import com.doubleslas.fifith.alcohol.ui.auth.CustomDialog
+import com.doubleslas.fifith.alcohol.ui.auth.CustomDialogInterface
 import com.doubleslas.fifith.alcohol.ui.auth.LoginActivity
-import com.doubleslas.fifith.alcohol.ui.auth.Register2Activity
-import com.doubleslas.fifith.alcohol.ui.auth.RegisterActivity
 import com.doubleslas.fifith.alcohol.ui.auth.recommendinfo.RecommendInfoActivity
 import com.doubleslas.fifith.alcohol.ui.main.MainActivity
 
@@ -23,7 +25,29 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val packageInfo =
+            applicationContext.packageManager.getPackageInfo(applicationContext.packageName, 0)
+        val versionCode =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+                packageInfo.longVersionCode
+            } else {
+                packageInfo.versionCode.toLong()
+            }
+        val minVersion = FirebaseConfig.getMinVersion()
 
+        if (versionCode < minVersion) {
+            CustomDialog(this, "버전 업데이트가 필요합니다.").apply {
+                customDialogInterface = object : CustomDialogInterface {
+                    override fun onConfirmBtnClicked() {
+                        // TODO : Play Store Open
+                        dismiss()
+                        finish()
+                    }
+                }
+                show()
+            }
+            return
+        }
 
         binding.animationView.addAnimatorListener(object : Animator.AnimatorListener {
             override fun onAnimationRepeat(animation: Animator?) {
